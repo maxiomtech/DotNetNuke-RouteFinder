0971896444039ce3a84646fef00c73a0a33d15cd
Jonathan Sheely <jsheely@twentytech.net>
2013-05-24 01:36:34 -0400

    Added type check for only MVC routes and not signalR

--- Views/Main.ascx.cs
+++ Views/Main.ascx.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+using System.Web.Routing;
 using InspectorIT.RouteFinder.Components.Common;
 
 namespace InspectorIT.RouteFinder.View
@@ -15,7 +12,7 @@ namespace InspectorIT.RouteFinder.View
 
             if (!Page.IsPostBack)
             {
-                rptRoutes.DataSource = System.Web.Routing.RouteTable.Routes;
+                rptRoutes.DataSource = RouteTable.Routes.OfType<Route>();
                 rptRoutes.DataBind();
             }
 
